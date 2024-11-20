956d0a020cbbd492b3616e82ffd6afa07930f8d0
João Vítor <48962164+jjoaovitor7@users.noreply.github.com>
2024-11-20 16:53:50 -0300

    feat: adding express with 2 routes.

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor escutando na porta ${PORT}.`);
+});
+
+app.get("/api", (req, res) => {
+    res.status(200).json({ message: "Hello World!" });
+});
